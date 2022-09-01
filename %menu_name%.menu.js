f614818ed7418d74eb5c2fca3c68b42dd55921a4
baeonette <baeonette@gmail.com>
2022-09-01 15:50:23 -0700

    Update %menu_name%.menu.js

--- %menu_name%.menu.js
+++ %menu_name%.menu.js
@@ -1,8 +1,8 @@
 module.exports = {
-    menu_id: "%menu_id%",
+    menu_id: "%menu_name%",
     menu_value: "%menu_option%",
-    description: "Option %menu_option% of menu %menu_id%",
+    description: "Option %menu_option% of menu %menu_name%",
     async execute(interaction, client) {
-        return interaction.reply({ content: 'Option %menu_option% was from %menu_id% was selected!', components: [], ephemeral: false });
+        return interaction.reply({ content: 'Option %menu_option% was from %menu_name% was selected!', components: [], ephemeral: false });
     }
 };
